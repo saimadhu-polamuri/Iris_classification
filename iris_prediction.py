f6045b625c2d099f7b3398d6238a5862296885f1
sai <sai@team.quadloops.com>
2015-05-27 09:05:48 +0530

    Implementaions of featurs graph was done.

--- iris_prediction.py
+++ iris_prediction.py
@@ -4,6 +4,7 @@ __autor__ = 'Saimadhu Polamuri'
 __website__ = 'www.dataaspirant.com'
 __createdon__ = '26-May-2015'
 
+__links__ = "http://matplotlib.org/users/screenshots.html"
 # Required pakages
 from sklearn import datasets
 import numpy as np
@@ -42,14 +43,29 @@ class IrisPrediction():
 		
 		return features_dict
 
-	def data_graph(self,x_name,y_name,x_points,y_points,colors_list,title_text):
+	def target_data_graph(self,target_data):
+
+		#fig = plt.figure()
+		labels = 'Iris_type-1', 'Iris_type-2', 'Iris_type-3'
+		sizes = list(set(target_data))
+		colors = ['yellowgreen', 'blue', 'red']
+		#explode = (0, 0.1, 0, 0) # only "explode" the 2nd slice (i.e. 'Hogs')
+
+		plt.pie(sizes, labels=labels, colors=colors)
+		# Set aspect ratio to be equal so that pie is drawn as a circle.
+		plt.axis('equal')
+
+		plt.show()
+		#fig.savefig("target_data.jpg")
+
+	def data_graph(self,x_name,y_name,x_points,y_points,colors_list,title_text,file_name):
 		
 		fig = plt.figure()
 		plt.scatter(x_points,y_points,color = colors_list)
 		fig.suptitle(title_text, fontsize=20)
 		plt.xlabel(x_name, fontsize=18)
 		plt.ylabel(y_name, fontsize=16)
-		fig.savefig('test1.jpg')
+		fig.savefig(file_name+'.jpg')
 		#return fig
 		#fig.show()
 	def call_data_graph(self,values_dict):
@@ -59,7 +75,14 @@ class IrisPrediction():
 		y_points = values_dict['sepal_width']
 		colors_list = values_dict['colors']
 		title_text = "sepal_length VS sepal_width Graph"
-		self.data_graph(x_name,y_name,x_points,y_points,colors_list,title_text)
+		self.data_graph(x_name,y_name,x_points,y_points,colors_list,title_text,'sepal_width-sepal_lenght_graph')
+		x_name = 'petal_length'
+		y_name = 'petal_width'
+		x_points = values_dict['petal_length']
+		y_points = values_dict['petal_width']
+		title_text = "petal_length VS petal_width Graph"
+		self.data_graph(x_name,y_name,x_points,y_points,colors_list,title_text,'petal_width-petal_lenght_graph')
+		
 
 
 
@@ -72,8 +95,8 @@ def main():
 	irisprediction = IrisPrediction()
 	#irisprediction.print_irisdata()
 	dict_values = irisprediction.separate_data()
-	irisprediction.call_data_graph(dict_values)
-
+	#irisprediction.call_data_graph(dict_values)
+	irisprediction.target_data_graph(dict_values['target'])
 
 if __name__ == "__main__":
 	main()
